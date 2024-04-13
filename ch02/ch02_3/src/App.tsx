4deac8d4e00524a790d8106c61a7eadffdf0f216
sylee <th42500@naver.com>
2024-04-13 18:07:56 +0900

    feat: 함수형 컴포넌트, 화살표형 컴포넌트 구현

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 함수형 컴포넌트
+/* export default function App() {
+  return <div>Hello function-keyword component!</div>
+} */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 화살표형 컴포넌트
+const App = () => {
+  return <h1>function component</h1>
 }
 
-export default App;
+export default App
