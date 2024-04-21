39aa19823992bc275ef0732333ca7510068c0674
sylee <th42500@naver.com>
2024-04-21 16:03:00 +0900

    feat: 물리 DOM 이벤츠 속성 - OnClick 이벤트 실습

--- ch02/ch02_5/src/pages/OnClick.tsx
+++ ch02/ch02_5/src/pages/OnClick.tsx
@@ -1,3 +1,18 @@
-export default function CopyMe() {
-  return <div>CopyMe</div>
+const rootDiv = document.getElementById('root')
+
+if(rootDiv) {
+  rootDiv.onclick = (e: Event) => {  // 호출x
+    const {isTrusted, target, bubbles} = e
+    console.log('mouse click occurs on rootDiv', isTrusted, target, bubbles)
+  }
+
+  rootDiv.onclick = (e: Event) => {  // 호출o ( 가장 마지막에 설정된 콜백함수만 호출)
+    const {isTrusted, target, bubbles} = e
+    // prettier-ignore
+    console.log('mouse click also occurs on rootDiv', isTrusted, target, bubbles)
+  }
+}
+
+export default function onClick() {
+  return <div>onClick</div>
 }
